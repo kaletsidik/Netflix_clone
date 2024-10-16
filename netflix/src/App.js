48ce6d5334840fa1a8065bdb2de23bec467cddf3
kaletsidik <awokeworket@gmail.com>
2024-10-16 19:39:40 +0300

    header and footer

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,5 +1,11 @@
+import Home from "./pages/Home/Home";
+import "./App.css";
 function App() {
-  return <div className="App">hey</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
